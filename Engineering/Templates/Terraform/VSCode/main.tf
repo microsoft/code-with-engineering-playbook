337588263275ed5191db77bf1f4abc74c39d3d3e
Charles Zipp <chzipp@microsoft.com>
2019-02-19 09:28:19 -0600

    remove formatting to prevent confusion over which extension is needed to support the formatting

--- Engineering/Templates/Terraform/VSCode/main.tf
+++ Engineering/Templates/Terraform/VSCode/main.tf
@@ -1,27 +1,28 @@
-provider "azurerm"{
-    version = "~>1.5"
+provider "azurerm" {
+  version = "~>1.5"
 }
-terraform{
-    backend "azurerm" {
-    }
+
+terraform {
+  backend "azurerm" {}
 }
 
 locals {
-    location_suffixes = {
-        centralus       = "cus"
-        eastus          = "eus"
-        eastus2         = "eus2"
-        westus          = "wus"
-        northcentralus  = "ncus"
-        southcentralus  = "scus"
-        westcentralus   = "wcus"
-        westus2         = "wus2"
-    }
-    location_suffix     = "${local.location_suffixes[var.location]}"
-    suffix              = "-core-${var.env}-${local.location_suffix}-${var.org}"
+  location_suffixes = {
+    centralus = "cus"
+    eastus = "eus"
+    eastus2 = "eus2"
+    westus = "wus"
+    northcentralus = "ncus"
+    southcentralus = "scus"
+    westcentralus = "wcus"
+    westus2 = "wus2"
+  }
+
+  location_suffix = "${local.location_suffixes[var.location]}"
+  suffix = "-core-${var.env}-${local.location_suffix}-${var.org}"
 }
 
 resource "azurerm_resource_group" "rg_core" {
-  name     = "rg${local.suffix}"
+  name = "rg${local.suffix}"
   location = "${var.location}"
 }
--- Engineering/Templates/Terraform/VSCode/variables.tf
+++ Engineering/Templates/Terraform/VSCode/variables.tf
@@ -1,12 +1,12 @@
 variable "location" {
-    type          = "string"
-    description   = "The name of the target location"
+    type = "string"
+    description = "The name of the target location"
 }
 variable "env" {
-    type        = "string",
+    type = "string",
     description = "The short name of the target env (i.e. dev, staging, or prod)"
 }
 variable "org" {
-    type        = "string",
+    type = "string",
     description = "The short name of the organization"
 }
